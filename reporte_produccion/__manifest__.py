5fb6b26a72c0e2849006572be9209747ae38fd1f
Unknown <pablo.osorio@xmarts.com>
2018-07-18 12:18:20 -0500

    Añadiendo archivos

---
+++ reporte_produccion/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Reporte por operación/empleado",
+
+    'summary': """
+       Reporte por operación/empleado""",
+
+    'description': """
+   Personalizaciones para BIBO
+    """,
+
+    'author': "Xmarts",
+    'website': "http://www.xmarts.com",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': [
+        'mrp'
+    ],
+    'data': [
+        'views/view.xml'
+    ],
+}
